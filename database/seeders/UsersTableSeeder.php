88072f5eff8d567bca92ee3a11ddc3390d0b6749
Nex Otaku <nex@otaku.ru>
2022-05-14 21:14:09 +0300

    Добавил сидер для демо пользователя

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::firstOrCreate(
+            [
+                'email' => 'demo@mail.com'
+            ],
+            [
+                'name' => 'Demo User',
+                'password' => bcrypt(config('secret')),
+            ]
+        );
+    }
+}
