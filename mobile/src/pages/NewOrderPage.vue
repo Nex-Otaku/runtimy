d12e6f907433dc07c8b5194acf77fe4f04f47da9
Nex Otaku <nex@otaku.ru>
2022-05-28 21:17:44 +0300

    Заменил заголовок на компонент

--- mobile/src/pages/NewOrderPage.vue
+++ mobile/src/pages/NewOrderPage.vue
@@ -1,48 +1,8 @@
 <template>
-  <q-header
-    class="bg-white text-black"
-    style="height: 83px"
-  >
-    <div
-      class="row q-pt-lg"
-    >
-      <q-toolbar
-        class="q-pa-none"
-        style="height: 100%"
-      >
-        <div class="col">
-          <q-btn
-            :to="{ name: 'orders' }"
-            flat
-            round
-            icon="west"
-          />
-        </div>
-        <div class="col q-pt-lg">
-          <q-toolbar-title
-            class="absolute-center"
-            style="font-size: 19px; font-weight: bold"
-          >
-            Новый заказ
-          </q-toolbar-title>
-        </div>
-        <div
-          class="col"
-          style="text-align: end"
-        >
-          <q-btn
-            flat
-            no-caps
-            @click="handleResetButtonClicked"
-          >
-            Очистить
-          </q-btn>
-        </div>
-      </q-toolbar>
-
-    </div>
-  </q-header>
-
+  <OrderFormHeader
+    :title="Новый заказ"
+    @reset="handleResetButtonClicked"
+  />
 
   <q-page-container>
 
