944954d132baff418e8452f295be7f51191752b6
Nex Otaku <nex@otaku.ru>
2022-06-01 12:11:18 +0300

    Сущность мобильного аккаунта

---
+++ app/Module/Customer/Entities/MobileAccount.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Module\Customer\Entities;
+
+use App\Models\MobileAccount as MobileAccountModel;
+use App\Models\User;
+
+class MobileAccount
+{
+    /** @var MobileAccountModel */
+    private $mobileAccount;
+
+    private function __construct(
+        MobileAccountModel $MobileAccount
+    ) {
+        $this->mobileAccount = $MobileAccount;
+    }
+
+    public static function register(string $phoneNumber): self
+    {
+        $user = new User();
+        $user->saveOrFail();
+
+        $mobileAccount = new MobileAccountModel(
+            [
+                'user_id' => $user->id,
+                'pincode' => null,
+                'phone_number' => $phoneNumber,
+            ]
+        );
+
+        $mobileAccount->saveOrFail();
+
+        return new self($mobileAccount);
+    }
+}
