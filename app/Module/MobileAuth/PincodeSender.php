6a6da4c7280bac83d00b475f1cadfe8d465cb849
Nex Otaku <nex@otaku.ru>
2022-06-08 14:57:28 +0300

    Подключил отправку СМС для пинкодов

--- app/Module/MobileAuth/PincodeSender.php
+++ app/Module/MobileAuth/PincodeSender.php
@@ -3,14 +3,15 @@
 namespace App\Module\MobileAuth;
 
 use App\Module\Common\PhoneNumber;
+use App\Module\Sms\SmsPilot;
 
 class PincodeSender
 {
     public function sendPincode(PhoneNumber $phoneNumber): string
     {
+        $pincode = random_int(1000, 9999);
+        SmsPilot::makeFromConfig()->sendSms($phoneNumber, 'Ваш код ' . $pincode);
 
-        // TODO
-
-        return '1234';
+        return $pincode;
     }
 }
