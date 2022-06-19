921f722e8dad3a8ed437e581d5832bb4341c8440
Nex Otaku <nex@otaku.ru>
2022-06-20 00:23:25 +0300

    Форматирование

--- app/Module/Payment/Entities/YookassaPayment.php
+++ app/Module/Payment/Entities/YookassaPayment.php
@@ -11,10 +11,10 @@ use YooKassa\Request\Payments\CreatePaymentResponse;
 
 class YookassaPayment
 {
-    private const STATUS_PENDING = 'pending';
+    private const STATUS_PENDING             = 'pending';
     private const STATUS_WAITING_FOR_CAPTURE = 'waiting_for_capture';
-    private const STATUS_SUCCEEDED = 'succeeded';
-    private const STATUS_CANCELED = 'canceled';
+    private const STATUS_SUCCEEDED           = 'succeeded';
+    private const STATUS_CANCELED            = 'canceled';
 
     private YookassaPaymentModel $yookassaPayment;
 
@@ -25,47 +25,48 @@ class YookassaPayment
     }
 
     public static function create(
-        int $shopId,
-        int $paymentId,
+        int                   $shopId,
+        int                   $paymentId,
         CreatePaymentResponse $paymentResponse,
-        string $returnUrl
-    ): self
-    {
+        string                $returnUrl
+    ): self {
         $metadata = $paymentResponse->metadata !== null
             ? Json::encode($paymentResponse->metadata->toArray())
             : null;
 
-        $yookassaPayment = new YookassaPaymentModel([
-                                                        'payment_id' => $paymentId,
-                                                        'shop_id' => $shopId,
-                                                        'recipient_account_id' => $paymentResponse->recipient->accountId,
-                                                        'recipient_gateway_id' => $paymentResponse->recipient->gatewayId,
-                                                        'payment_method_type' => $paymentResponse->paymentMethod?->getType(),
-                                                        'payment_method_title' => $paymentResponse->paymentMethod?->title,
-                                                        'amount' => $paymentResponse->amount->value,
-                                                        'currency' => $paymentResponse->amount->currency,
-                                                        'income_amount' => $paymentResponse->incomeAmount?->value,
-                                                        'income_currency' => $paymentResponse->incomeAmount?->currency,
-                                                        'confirmation_type' => $paymentResponse->confirmation->type,
-                                                        'return_url' => $returnUrl,
-                                                        'confirmation_url' => $paymentResponse->confirmation->getConfirmationUrl(),
-                                                        'description' => $paymentResponse->description,
-                                                        'external_id' => $paymentResponse->id,
-                                                        'status' => $paymentResponse->status,
-                                                        'paid' => $paymentResponse->paid ? 1 : 0,
-                                                        'refundable' => $paymentResponse->refundable ? 1 : 0,
-                                                        'refunded_amount' => $paymentResponse->refundedAmount?->value,
-                                                        'refunded_currency' => $paymentResponse->refundedAmount?->currency,
-                                                        'test' => $paymentResponse->test ? 1 : 0,
-                                                        'captured_at' => $paymentResponse->capturedAt,
-                                                        'created_at' => $paymentResponse->createdAt,
-                                                        'expires_at' => $paymentResponse->expiresAt,
-                                                        'receipt_registration' => $paymentResponse->receiptRegistration,
-                                                        'metadata' => $metadata,
-                                                        'cancellation_party' => $paymentResponse->cancellationDetails?->party,
-                                                        'cancellation_reason' => $paymentResponse->cancellationDetails?->reason,
-                                                        'error' => '',
-                                                    ]);
+        $yookassaPayment = new YookassaPaymentModel(
+            [
+                'payment_id' => $paymentId,
+                'shop_id' => $shopId,
+                'recipient_account_id' => $paymentResponse->recipient->accountId,
+                'recipient_gateway_id' => $paymentResponse->recipient->gatewayId,
+                'payment_method_type' => $paymentResponse->paymentMethod?->getType(),
+                'payment_method_title' => $paymentResponse->paymentMethod?->title,
+                'amount' => $paymentResponse->amount->value,
+                'currency' => $paymentResponse->amount->currency,
+                'income_amount' => $paymentResponse->incomeAmount?->value,
+                'income_currency' => $paymentResponse->incomeAmount?->currency,
+                'confirmation_type' => $paymentResponse->confirmation->type,
+                'return_url' => $returnUrl,
+                'confirmation_url' => $paymentResponse->confirmation->getConfirmationUrl(),
+                'description' => $paymentResponse->description,
+                'external_id' => $paymentResponse->id,
+                'status' => $paymentResponse->status,
+                'paid' => $paymentResponse->paid ? 1 : 0,
+                'refundable' => $paymentResponse->refundable ? 1 : 0,
+                'refunded_amount' => $paymentResponse->refundedAmount?->value,
+                'refunded_currency' => $paymentResponse->refundedAmount?->currency,
+                'test' => $paymentResponse->test ? 1 : 0,
+                'captured_at' => $paymentResponse->capturedAt,
+                'created_at' => $paymentResponse->createdAt,
+                'expires_at' => $paymentResponse->expiresAt,
+                'receipt_registration' => $paymentResponse->receiptRegistration,
+                'metadata' => $metadata,
+                'cancellation_party' => $paymentResponse->cancellationDetails?->party,
+                'cancellation_reason' => $paymentResponse->cancellationDetails?->reason,
+                'error' => '',
+            ]
+        );
 
         $yookassaPayment->saveOrFail();
 
@@ -106,11 +107,11 @@ class YookassaPayment
     public function canSucceed(): bool
     {
         return in_array(
-                $this->yookassaPayment->status,
-                [
-                    self::STATUS_PENDING,
-                    self::STATUS_WAITING_FOR_CAPTURE,
-                ]
+            $this->yookassaPayment->status,
+            [
+                self::STATUS_PENDING,
+                self::STATUS_WAITING_FOR_CAPTURE,
+            ]
         );
     }
 
