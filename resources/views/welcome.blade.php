0733ab771171c5df2385bfd4fff4f5fe1899e3fd
Nex Otaku <nex@otaku.ru>
2022-07-12 20:54:59 +0300

    Улучшил текст на странице входа в ЛК

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,12 +34,12 @@
 
                             @if (Route::has('nova.login'))
                                     @auth
-                                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                                        <a href="{{ url('/n') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Дашборд</a>
                                     @else
-                                        <a href="{{ route('nova.login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                                        <a href="{{ route('nova.login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Войти</a>
 
                                         @if (Route::has('register'))
-                                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Зарегистрироваться</a>
                                         @endif
                                     @endauth
                             @endif
